7bc5d3e6ae9059f4043008913c3b9dc7723eb042
PWiatros <pawelwiatros@o2.pl>
2021-07-15 11:51:17 +0200

    stworzenie

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let number = 12;
+
+if (number === 12) {
+    console.log('Podana liczba jest większa od 10');
+}else if (number === 12) {
+    console.log('Podana liczba nie jest większa od 10 ')
+}
